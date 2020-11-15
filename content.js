0632387e39c6bbd735d32995867198f819a7e5f7
Felix Yeung <FelixYeung@Felixs-MacBook-Pro.local>
2020-11-15 17:06:14 +0800

    History now updates regularly

--- content.js
+++ content.js
@@ -1,3 +1,22 @@
-chrome.storage.sync.get("extensionHistory", function (result) {
-    window.localStorage.extensionHistory = result["extensionHistory"] || "[]";
-});
+let sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function updateLocalStorage() {
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.sync.get("extensionHistory", function (result) {
+                window.localStorage.extensionHistory =
+                    result["extensionHistory"] || "[]";
+            });
+            resolve();
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+(async function () {
+    while (true) {
+        await updateLocalStorage();
+        await sleep(5000);
+    }
+})();
